7007dc091ea07850c59219a274bef70705a1a0a0
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-08 19:05:00 +0530

    Created the Constants singleton(Kotlin Object).

---
+++ app/src/main/java/com/example/nishant/fenrir/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.nishant.fenrir.util
+
+import org.threeten.bp.LocalDate
+
+object Constants {
+
+    val festDates = listOf(
+            LocalDate.of(2018, 10, 27),
+            LocalDate.of(2018, 10, 28),
+            LocalDate.of(2018, 10, 29),
+            LocalDate.of(2018, 10, 30),
+            LocalDate.of(2018, 10, 31)
+    )
+}
