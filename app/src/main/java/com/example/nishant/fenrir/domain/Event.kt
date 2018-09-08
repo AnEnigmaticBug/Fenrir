318888d616b6d7c884aab31dceb91e4b50ea56ff
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-08 17:57:59 +0530

    Made the Venue, Category and the Event classes.

---
+++ app/src/main/java/com/example/nishant/fenrir/domain/Event.kt
@@ -0,0 +1,10 @@
+package com.example.nishant.fenrir.domain
+
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalTime
+
+/**
+ * @property time is the starting time of the event.
+ * @property duration is the duration of the event in minutes.
+ * */
+data class Event(val id: String, val name: String, val about: String, val rules: String, val venue: Venue?, val category: Category, val date: LocalDate?, val time: LocalTime?, val duration: Int?, val subscribed: Boolean)
---
+++ app/src/main/java/com/example/nishant/fenrir/domain/Venue.kt
@@ -0,0 +1,6 @@
+package com.example.nishant.fenrir.domain
+
+enum class Venue {
+    // TODO: Add all the required Venues.
+    Audi, MLawns, Rotunda, Stalls, SACHall
+}
