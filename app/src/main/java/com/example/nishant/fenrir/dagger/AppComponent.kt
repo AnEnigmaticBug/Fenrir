bf564c1a9637aaeb801ec34cc1a72657f8cf1806
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-10 16:06:03 +0530

    Made the EventListViewModelFactory.

--- app/src/main/java/com/example/nishant/fenrir/dagger/AppComponent.kt
+++ app/src/main/java/com/example/nishant/fenrir/dagger/AppComponent.kt
@@ -1,7 +1,11 @@
 package com.example.nishant.fenrir.dagger
 
+import com.example.nishant.fenrir.eventlist.EventListViewModelFactory
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton @Component(modules = [AppModule::class])
-abstract class AppComponent
+abstract class AppComponent {
+
+    abstract fun inject(viewModelFactory: EventListViewModelFactory)
+}
--- app/src/main/java/com/example/nishant/fenrir/dagger/AppModule.kt
+++ app/src/main/java/com/example/nishant/fenrir/dagger/AppModule.kt
@@ -1,12 +1,22 @@
 package com.example.nishant.fenrir.dagger
 
 import android.content.Context
+import com.example.nishant.fenrir.data.CentralRepository
+import com.example.nishant.fenrir.data.CentralRepositoryImpl
+import com.example.nishant.fenrir.data.EventRepository
+import com.example.nishant.fenrir.data.StubEventRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
 class AppModule(private val context: Context) {
+
+    @Provides @Singleton
+    fun providesEventRepository(): EventRepository = StubEventRepositoryImpl()
+
+    @Provides @Singleton
+    fun providesCentralRepository(context: Context): CentralRepository = CentralRepositoryImpl(context)
 
     @Provides @Singleton
     fun providesContext(): Context = context
