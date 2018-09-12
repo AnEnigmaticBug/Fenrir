0eacef520f99752bff2b268e0989a2d009a360d8
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-12 20:35:54 +0530

    Created the Layout and Fragment files for EventFilterMenu screen.

---
+++ app/src/main/java/com/example/nishant/fenrir/events/eventfilter/EventFilterMenuFragment.kt
@@ -0,0 +1,19 @@
+package com.example.nishant.fenrir.events.eventfilter
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nishant.fenrir.R
+
+class EventFilterMenuFragment : Fragment() {
+
+    private lateinit var rootPOV: View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        rootPOV = inflater.inflate(R.layout.fra_event_filter_menu, container, false)
+
+        return rootPOV
+    }
+}
