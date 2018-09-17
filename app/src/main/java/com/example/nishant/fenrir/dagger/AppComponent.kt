f1d6238542ef2182aeffe6fcba8af55704266b1d
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-17 23:38:27 +0530

    Made the ViewModelFactory for EventFilterList screen and set it up with Dagger2.

--- app/src/main/java/com/example/nishant/fenrir/dagger/AppComponent.kt
+++ app/src/main/java/com/example/nishant/fenrir/dagger/AppComponent.kt
@@ -1,5 +1,6 @@
 package com.example.nishant.fenrir.dagger
 
+import com.example.nishant.fenrir.events.eventfilter.eventfilterlist.EventFilterListViewModelFactory
 import com.example.nishant.fenrir.events.eventfilter.eventfiltermenu.EventFilterMenuViewModelFactory
 import com.example.nishant.fenrir.events.eventinfo.EventInfoViewModelFactory
 import com.example.nishant.fenrir.events.eventlist.EventListViewModelFactory
@@ -14,4 +15,6 @@ abstract class AppComponent {
     abstract fun inject(viewModelFactory: EventInfoViewModelFactory)
 
     abstract fun inject(viewModelFactory: EventFilterMenuViewModelFactory)
+
+    abstract fun inject(viewModelFactory: EventFilterListViewModelFactory)
 }
---
+++ app/src/main/java/com/example/nishant/fenrir/events/eventfilter/eventfilterlist/EventFilterListViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.example.nishant.fenrir.events.eventfilter.eventfilterlist
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.example.nishant.fenrir.FenrirApp
+import com.example.nishant.fenrir.data.CentralRepository
+import javax.inject.Inject
+
+class EventFilterListViewModelFactory(private val type: FilterType) : ViewModelProvider.Factory {
+
+    @Inject
+    lateinit var centralRepository: CentralRepository
+
+    init {
+        FenrirApp.appComponent.inject(this)
+    }
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return EventFilterListViewModel(centralRepository, type) as T
+    }
+}
