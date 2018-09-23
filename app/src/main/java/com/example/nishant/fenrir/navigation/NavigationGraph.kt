4431ee35557708ff5f0994afff26edd3e352ac4b
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-23 19:50:36 +0530

    Made slow versions of fade-in and fade-out animations. Used them for FilterMenu.

--- app/src/main/java/com/example/nishant/fenrir/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/nishant/fenrir/navigation/NavigationGraph.kt
@@ -11,8 +11,8 @@ object NavigationGraph {
 
         object EventFilter {
 
-            val FILTER_MENU = Destination("FILTER_MENU", 0, 0, 0, 0)
-            val FILTER_LIST = Destination("FILTER_LIST", 0, 0, 0, 0)
+            val FILTER_MENU = Destination("FILTER_MENU", 0, 0, R.anim.an_fade_in_slow, R.anim.an_fade_out_slow)
+            val FILTER_LIST = Destination("FILTER_LIST", R.anim.an_fade_in_slow, R.anim.an_fade_out_slow, 0, 0)
         }
     }
 
