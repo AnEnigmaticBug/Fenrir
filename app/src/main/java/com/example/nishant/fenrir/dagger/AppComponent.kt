a785ecffb9ff476301afb47715c2548ded71ecf3
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-09-29 14:00:11 +0530

    Made the ViewModelFactory for the Cart screen. Integrated it and CartEntriesAdapter into CartFragment.

--- app/src/main/java/com/example/nishant/fenrir/dagger/AppComponent.kt
+++ app/src/main/java/com/example/nishant/fenrir/dagger/AppComponent.kt
@@ -4,6 +4,7 @@ import com.example.nishant.fenrir.screens.mainapp.events.eventfilter.eventfilter
 import com.example.nishant.fenrir.screens.mainapp.events.eventfilter.eventfiltermenu.EventFilterMenuViewModelFactory
 import com.example.nishant.fenrir.screens.mainapp.events.eventinfo.EventInfoViewModelFactory
 import com.example.nishant.fenrir.screens.mainapp.events.eventlist.EventListViewModelFactory
+import com.example.nishant.fenrir.screens.wallet.cart.CartViewModelFactory
 import com.example.nishant.fenrir.screens.wallet.items.ItemsViewModelFactory
 import com.example.nishant.fenrir.screens.wallet.stalls.StallsViewModelFactory
 import dagger.Component
@@ -23,4 +24,6 @@ abstract class AppComponent {
     abstract fun inject(viewModelFactory: StallsViewModelFactory)
 
     abstract fun inject(viewModelFactory: ItemsViewModelFactory)
+
+    abstract fun inject(viewModelFactory: CartViewModelFactory)
 }
--- app/src/main/java/com/example/nishant/fenrir/screens/wallet/cart/CartFragment.kt
+++ app/src/main/java/com/example/nishant/fenrir/screens/wallet/cart/CartFragment.kt
@@ -1,18 +1,70 @@
 package com.example.nishant.fenrir.screens.wallet.cart
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.nishant.fenrir.R
+import kotlinx.android.synthetic.main.fra_cart.view.*
 
-class CartFragment : Fragment() {
+class CartFragment : Fragment(), CartEntriesAdapter.ClickListener {
 
+    private lateinit var viewModel: CartViewModel
     private lateinit var rootPOV: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewModel = ViewModelProviders.of(this, CartViewModelFactory())[CartViewModel::class.java]
+
         rootPOV = inflater.inflate(R.layout.fra_cart, container, false)
+
+        rootPOV.cartEntriesRCY.adapter = CartEntriesAdapter(this)
+
+        rootPOV.payBTN.setOnClickListener {
+            viewModel.onPayButtonClicked()
+        }
+
+        viewModel.rawOrders.observe(this, Observer {
+            (rootPOV.cartEntriesRCY.adapter as CartEntriesAdapter).entries = it?: listOf()
+        })
+
+        viewModel.totalCost.observe(this, Observer {
+            rootPOV.totalAmountLBL.text = it?: "Total Amount: INR 0"
+        })
+
+        viewModel.buyStatus.observe(this, Observer {
+            when(it!!) {
+                is BuyAttemptStatus.Idle       -> {
+                    showInProgressStuff(false)
+                }
+                is BuyAttemptStatus.InProgress -> {
+                    showInProgressStuff(true)
+                }
+                is BuyAttemptStatus.Success    -> {
+                    Toast.makeText(this.requireActivity(), "Order placed successfully", Toast.LENGTH_LONG).show()
+                }
+                is BuyAttemptStatus.Failure    -> {
+                    Toast.makeText(this.requireActivity(), (it as BuyAttemptStatus.Failure).message , Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
         return rootPOV
+    }
+
+    private fun showInProgressStuff(show: Boolean) {
+        val visibility = when(show) {
+            true  -> View.VISIBLE
+            false -> View.GONE
+        }
+        rootPOV.screenFaderPOV.visibility = visibility
+        rootPOV.inProgressLOT.visibility = visibility
+    }
+
+    override fun onOrderRemoveButtonClicked(orderId: String) {
+        viewModel.onOrderRemoved(orderId)
     }
 }
