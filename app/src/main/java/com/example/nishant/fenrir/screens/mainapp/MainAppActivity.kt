5bd7e76963c11a22a09eee32460876c79058bd9d
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-10-18 21:10:12 +0530

    BugFix: Now, the mainapp's screens are in sync with highlighted bottom nav icon.

--- app/src/main/java/com/example/nishant/fenrir/screens/mainapp/MainAppActivity.kt
+++ app/src/main/java/com/example/nishant/fenrir/screens/mainapp/MainAppActivity.kt
@@ -14,7 +14,6 @@ import kotlinx.android.synthetic.main.act_main.*
 class MainAppActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainAppViewModel
-    private var currentTabIndex = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,22 +25,37 @@ class MainAppActivity : AppCompatActivity() {
     }
 
     private fun setupBottomNav() {
+        findNavController(R.id.navHostFRA).addOnNavigatedListener { controller, destination ->
+            when(destination.id) {
+                R.id.outsteeLoginFragment, R.id.profileFragment       -> {
+                    if(bottomNavAHB.currentItem != 0) {
+                        bottomNavAHB.setCurrentItem(0, false)
+                    }
+                }
+                R.id.eventListFragment, R.id.eventInfoFragment        -> {
+                    if(bottomNavAHB.currentItem != 1) {
+                        bottomNavAHB.setCurrentItem(1, false)
+                    }
+                }
+                R.id.mapFragment                                      -> {
+                    if(bottomNavAHB.currentItem != 3) {
+                        bottomNavAHB.setCurrentItem(3, false)
+                    }
+                }
+                R.id.moreFragment                                     -> {
+                    if(bottomNavAHB.currentItem != 4) {
+                        bottomNavAHB.setCurrentItem(4, false)
+                    }
+                }
+            }
+        }
+
         AHBottomNavigationAdapter(this, R.menu.mn_bottom_nav_main_app).setupWithBottomNavigation(bottomNavAHB)
         bottomNavAHB.titleState = AHBottomNavigation.TitleState.ALWAYS_SHOW
         bottomNavAHB.accentColor = resources.getColor(R.color.vio03)
-        bottomNavAHB.setCurrentItem(currentTabIndex, false)
+        bottomNavAHB.setCurrentItem(1, true)
 
         bottomNavAHB.setOnTabSelectedListener { position, wasSelected ->
-            if(position == 2) {
-                bottomNavAHB.setCurrentItem(currentTabIndex, false)
-            }
-            else {
-                currentTabIndex = position
-            }
-            if(wasSelected) {
-                Toast.makeText(applicationContext, "Already at the tab", Toast.LENGTH_SHORT).show()
-            }
-            else {
                 val options = NavOptions.Builder()
                         .setLaunchSingleTop(true)
                         .setPopUpTo(R.id.eventListFragment, false).build()
@@ -58,6 +72,8 @@ class MainAppActivity : AppCompatActivity() {
                             true  -> {
                                 bottomNavAHB.setCurrentItem(0, false)
                                 findNavController(R.id.navHostFRA).navigate(R.id.outsteeLoginFragment, null, options)
+                                Toast.makeText(this, "You need to log in to use Wallet", Toast.LENGTH_LONG).show()
+                                bottomNavAHB.setCurrentItem(0, true)
                             }
                             false -> findNavController(R.id.navHostFRA).navigate(R.id.walletActivity, null, options)
                         }
@@ -66,7 +82,6 @@ class MainAppActivity : AppCompatActivity() {
                     4    -> findNavController(R.id.navHostFRA).navigate(R.id.moreFragment, null, options)
                     else -> throw IllegalStateException("$position th bottom nav tab was selected")
                 }
-            }
             true
         }
     }
